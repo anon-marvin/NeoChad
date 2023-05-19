44953636bb706f206e19251a58fd85c56a7911ca
anon-marvin <108331198+anon-marvin@users.noreply.github.com>
2023-05-19 22:08:20 +0530

    Lsp Zero Removed

--- lua/marvin/option.lua
+++ lua/marvin/option.lua
@@ -11,11 +11,9 @@ vim.opt.wrap = true
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undofile = true
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
 vim.opt.termguicolors = true
 vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
 vim.opt.updatetime = 50
 vim.opt.termguicolors = true
+vim.opt.cursorline = true
 
